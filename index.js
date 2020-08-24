4e54995ce0939169df01969456c9b418536484ae
= <=>
2020-08-24 07:01:17 +0100

    he has help command

--- index.js
+++ index.js
@@ -2,4 +2,17 @@ const Discord = require(`discord.js`);
 const client = new Discord.Client();
 const config = require(`./config.json`);
 
+client.once(`ready`, () => {
+    console.log(`Bit64ON`);
+    client.user.setActivity(`bits to your Discord!`, {type: `STREAMING`});
+});
+
+client.on(`message`, message => {
+    if(message.author.bot || !message.content.startsWith(config.prefix)) return;
+    const args = message.content.slice(config.prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    if(command === `help`) message.channel.send(`No commands yet.`);
+})
+
+
 client.login(config.token);
